6268aac2f07dea446917f4ad92561a70b8e52b8a
namanegi <namanegippoi@gmail.com>
2020-12-02 16:14:16 +0900

    1

--- js1130/dist/js/load.js
+++ js1130/dist/js/load.js
@@ -36,4 +36,18 @@ $(function() {
             };
         };
     });
+    function compareName(obj1, obj2) {
+        return (obj1.kastKana + obj1.firstKana) < (obj2.kastKana + obj2.firstKana);
+    }
+    $('#sorting-name').click(function() {
+        $db.sort(compareName);
+        for (var i in $db) {
+            $('#loadarea').append('<li class="user-box col-md-2 col-sm-4 col-xs-12 text-center"><div class="thumbnail"><div class="thumbnail-image"><img class="img-circle" src="' + 
+            $db[i].imagePath + '"></div><div class="user-indicator"></div><div class="user-name">' + 
+            $db[i].lastName + $db[i].firstName + '</div><div class="user-division">' + 
+            $db[i].division + '</div><div class="user-position">' + $db[i].position + '</div>' + 
+            '<div class="text-center"></div></div></li>');
+        };
+        $.getJSON("dist/js/include.json", function(data) {$db = data;});
+    });
 });
