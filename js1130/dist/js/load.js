c3d223e119a3f50ffaac175064d282558d97f010
namanegi <namanegippoi@gmail.com>
2020-12-02 16:31:26 +0900

    1

--- js1130/dist/js/load.js
+++ js1130/dist/js/load.js
@@ -12,16 +12,6 @@ $(function() {
             };
         });
     });
-    $('#sorting-clear').click(function() {
-        $('#loadarea').empty();
-        for (var i in $db) {
-            $('#loadarea').append('<li class="user-box col-md-2 col-sm-4 col-xs-12 text-center"><div class="thumbnail"><div class="thumbnail-image"><img class="img-circle" src="' + 
-            $db[i].imagePath + '"></div><div class="user-indicator"></div><div class="user-name">' + 
-            $db[i].lastName + $db[i].firstName + '</div><div class="user-division">' + 
-            $db[i].division + '</div><div class="user-position">' + $db[i].position + '</div>' + 
-            '<div class="text-center"></div></div></li>');
-        };
-    });
     $('#query-division').change(function() {
         if ($('#query-division').val() != "") {
             $('#loadarea').empty();
@@ -44,7 +34,34 @@ $(function() {
             return 1
         };
         return 0;
-    }
+    };
+    function compareHireday(obj1, obj2) {
+        if ((obj1.hireday) < (obj2.hireday)) {
+            return -1
+        };
+        if ((obj1.hireday) > (obj2.hireday)) {
+            return 1
+        };
+        return 0;
+    };
+    function compareBirthday(obj1, obj2) {
+        if ((obj1.birthday) < (obj2.birthday)) {
+            return -1
+        };
+        if ((obj1.birthday) > (obj2.birthday)) {
+            return 1
+        };
+        return 0;
+    };
+    function compareNo(obj1, obj2) {
+        if ((obj1.number) < (obj2.number)) {
+            return -1
+        };
+        if ((obj1.number) > (obj2.number)) {
+            return 1
+        };
+        return 0;
+    };
     $('#sorting-name').click(function() {
         $('#loadarea').empty();
         $db.sort(compareName);
@@ -55,6 +72,38 @@ $(function() {
             $db[i].division + '</div><div class="user-position">' + $db[i].position + '</div>' + 
             '<div class="text-center"></div></div></li>');
         };
-        $.getJSON("dist/js/include.json", function(data) {$db = data;});
+    });
+    $('#sorting-hireday').click(function() {
+        $('#loadarea').empty();
+        $db.sort(compareHireday);
+        for (var i in $db) {
+            $('#loadarea').append('<li class="user-box col-md-2 col-sm-4 col-xs-12 text-center"><div class="thumbnail"><div class="thumbnail-image"><img class="img-circle" src="' + 
+            $db[i].imagePath + '"></div><div class="user-indicator"></div><div class="user-name">' + 
+            $db[i].lastName + $db[i].firstName + '</div><div class="user-division">' + 
+            $db[i].division + '</div><div class="user-position">' + $db[i].position + '</div>' + 
+            '<div class="text-center"></div></div></li>');
+        };
+    });
+    $('#sorting-birthday').click(function() {
+        $('#loadarea').empty();
+        $db.sort(compareBirthday);
+        for (var i in $db) {
+            $('#loadarea').append('<li class="user-box col-md-2 col-sm-4 col-xs-12 text-center"><div class="thumbnail"><div class="thumbnail-image"><img class="img-circle" src="' + 
+            $db[i].imagePath + '"></div><div class="user-indicator"></div><div class="user-name">' + 
+            $db[i].lastName + $db[i].firstName + '</div><div class="user-division">' + 
+            $db[i].division + '</div><div class="user-position">' + $db[i].position + '</div>' + 
+            '<div class="text-center"></div></div></li>');
+        };
+    });
+    $('#sorting-clear').click(function() {
+        $('#loadarea').empty();
+        $db.sort(compareNo);
+        for (var i in $db) {
+            $('#loadarea').append('<li class="user-box col-md-2 col-sm-4 col-xs-12 text-center"><div class="thumbnail"><div class="thumbnail-image"><img class="img-circle" src="' + 
+            $db[i].imagePath + '"></div><div class="user-indicator"></div><div class="user-name">' + 
+            $db[i].lastName + $db[i].firstName + '</div><div class="user-division">' + 
+            $db[i].division + '</div><div class="user-position">' + $db[i].position + '</div>' + 
+            '<div class="text-center"></div></div></li>');
+        };
     });
 });
